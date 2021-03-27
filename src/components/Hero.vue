12fd63c3feb29b022b67259dec1ad415535ecc22
Wizer <simon.tessier8@gmail.com>
2021-03-27 13:17:18 +0100

    Text animation

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -3,6 +3,10 @@
     <div id="center_bubble">
       <span id="bubble1">      
       </span>
+      <span id="bubble2">      
+      </span>
+      <span id="bubble3">      
+      </span>
     </div>
     <div id="hero_header">
       <a href="" id="projects_button">
@@ -10,9 +14,16 @@
       </a>
     </div>
     <div id="hero_body">
-      <h1 id="title">
-        Simon
-      </h1> 
+      <div id="hero_title_holder">
+        <p id="title_white">
+          Simon
+        </p> 
+        <div id="plane_gray">
+        </div> 
+        <p id="title_translate">
+          Simon
+        </p> 
+      </div>
       <div id="sub_title">
         <div id="creative_stack">
         </div>
@@ -48,15 +59,59 @@ export default {
     // Create bubbles
     let hero = document.getElementById('hero')
     let bubble1 = document.getElementById('bubble1')
+    let bubble2 = document.getElementById('bubble2')
+    let bubble3 = document.getElementById('bubble3')
+
+    let title_white = document.getElementById('title_white')
+    let plane_gray = document.getElementById('plane_gray')
+    let title_translate = document.getElementById('title_translate')
+
+    let last_x = 0
+    let last_y = 0
+
     hero.addEventListener('mousemove', event => {
       let square = hero.getBoundingClientRect()
       let x = event.offsetX - square.width/2
       let y = event.offsetY - square.height/2
 
       bubble1.style.transform = `translate(${-x}px, ${-y}px)`
+      bubble2.style.transform = `translate(${-y * 0.8}px, ${-x * 0.5}px)`
+      bubble3.style.transform = `translate(${-x / 2}px, ${-y / 2}px)`
 
-      console.log(x, y)
+      // Main Title effect
+      let title_rect = title_white.getBoundingClientRect()
+      plane_gray.style.height = `${title_rect.height}px`
+      plane_gray.style.width = `${title_rect.width}px`
 
+      if (
+        title_rect.left < event.offsetX && 
+        event.offsetX < title_rect.right &&
+        title_rect.top < event.offsetY &&
+        event.offsetY < title_rect.bottom
+        ){
+        // Gray circle
+        let local_x = event.offsetX - title_rect.left
+        let local_y = event.offsetY - title_rect.top
+
+        let x_percent = local_x / (title_rect.width / 100) 
+        let y_percent = local_y / (title_rect.height / 100) 
+        
+        plane_gray.style.clipPath = `circle(20% at ${x_percent}% ${y_percent}%)`
+
+        // Text translate
+        title_translate.style.transform = `translate(${local_x - last_x}px, ${local_y - last_y}px)`
+        title_translate.style.clipPath = `circle(20% at ${x_percent}% ${y_percent}%)`
+        
+        last_x = local_x
+        last_y = local_y
+      }
+      else{
+        // Reset positions
+        plane_gray.style.clipPath = `circle(0% at 50% 50%)`
+
+        title_translate.style.transform = `translate(0px, 0px)`
+        title_translate.style.clipPath = `circle(0% at 50% 50%)`
+      }
     })
   }
 }
@@ -84,13 +139,34 @@ export default {
 {
   position: absolute;
   margin: 3em;
-
 }
-#title
+/* Titles */
+#hero_title_holder
+{
+  display: grid;
+}
+#hero_title_holder p
 {  
   font-size: 15em;
   margin: 0px;
+
+  grid-column: 1;
+  grid-row: 1;
 }
+#title_translate
+{
+  transition-duration: 300ms;
+  transition-timing-function: ease-out;
+}
+#plane_gray
+{
+  position: absolute;
+
+  background-color: #262626;
+  transition-duration: 300ms;
+  transition-timing-function: ease-out;
+}
+/* Subs */
 #sub_title
 {  
   display: flex;
@@ -105,6 +181,7 @@ export default {
 #creative_stack
 {
   display: grid;
+  grid-template-rows: repeat(1, 1fr);
 }
 /*  Bubbles */
 #center_bubble
@@ -115,6 +192,7 @@ export default {
   align-items: center;
   width: 100vw;
   height: 100vh;
+  pointer-events: none;
 }
 #bubble1
 {
@@ -122,7 +200,33 @@ export default {
   height: 2em;
   width: 2em;
   background-color: red;
-  pointer-events: none;
+  border-radius: 50%;
+  transition-duration: 600ms;
+  transition-timing-function: ease-out;
+  mix-blend-mode: hard-light;
+}
+#bubble2
+{
+  position: absolute;
+  height: 3em;
+  width: 3em;
+  background-color: rgb(116, 77, 221);
+  border-radius: 50%;
+  transition-duration: 900ms;
+  transition-timing-function: ease-out;
+  mix-blend-mode: hard-light;
+}
+#bubble3
+{
+  position: absolute;
+  height: 1em;
+  width: 1em;
+  background-color: rgb(101, 113, 223);
+  border-radius: 50%;
+
+  transition-duration: 300ms;
+  transition-timing-function: ease-out;
+  mix-blend-mode: hard-light;
 }
 
 </style>
