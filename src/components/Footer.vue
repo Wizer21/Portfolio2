cf53879ac022ea3bd7099e11749c4186677c9c9e
Wizer <simon.tessier8@gmail.com>
2021-03-30 19:55:44 +0200

    Footer animation

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,13 +3,21 @@
     <p id="text">
       If you want me to join your team you can contact me at  
     </p>
-    <div id="mail" @click="clipboard">
-      <p id="top_text">        
-        simon.tessier8@gmail.com
-      </p>
-      <p id="bottom_text">        
-        simon.tessier8@gmail.com
-      </p>
+    <div class="holder" id="gmail_holder">
+      <h2>
+        Gmail
+      </h2>
+      <h2 id="gmail">
+        Gmail
+      </h2>
+    </div>
+    <div class="holder" id="twitter_holder">
+      <h2>
+        Twitter
+      </h2>
+      <h2 id="twitter">
+        Twitter
+      </h2>
     </div>
   </div>
 </template>
@@ -21,7 +29,38 @@ export default {
     clipboard(){
       navigator.clipboard.writeText('simon.tessier8@gmail.com');
     }
+  },
+  mounted(){
+    let holders = document.getElementsByClassName('holder')
+    for (let holder of holders){
+      holder.addEventListener('mouseenter', event => {
+        let square = holder.getBoundingClientRect()
+        let x_percent = event.offsetX / (square.width / 100)
+        let y_percent = event.offsetY / (square.height / 100)
+
+        let child = holder.children[1]
+        child.style.transitionDuration = "0ms"
+        child.style.clipPath = `circle(0% at ${x_percent}% ${y_percent}%)`
+
+        setTimeout(() => {          
+          child.style.transitionDuration = "500ms"
+          child.style.clipPath = `circle(150% at ${x_percent}% ${y_percent}%)`
+        }, 50)
+      })
+    }
+    for (let holder of holders){
+      holder.addEventListener('mouseleave', event => {
+        let square = holder.getBoundingClientRect()
+        let x_percent = event.offsetX / (square.width / 100)
+        let y_percent = event.offsetY / (square.height / 100)
+
+        let child = holder.children[1]
+        child.style.transitionDuration = "500ms"
+        child.style.clipPath = `circle(0% at ${x_percent}% ${y_percent}%)`
+      })
+    }
   }
+  
 }
 </script>
 
@@ -32,42 +71,44 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
 }
 #text
 {
   font-size: 2vw;
 }
-#mail
+.holder
 {
   display: grid;
-}
-#top_text
-{
-  font-size: 4em;
-  margin: 3vw;
-  color: #e53935;
+  align-items: center;
+  justify-content: center;
+  color: #1a1a1a;
+  transition-duration: 1000ms;
 
+}
+#gmail_holder
+{
+  margin-right: 30vw;
+}
+#twitter_holder
+{
+  margin-left: 30vw;
+}
+.holder h2
+{
   grid-column: 1;
   grid-row: 1;
-
-  z-index: 2;
-
-  clip-path: polygon(0% 0%, 75% 0%, 50% 100%, 0% 100%);
-  transition-duration: 500ms;
-  pointer-events: none;
+  font-size: 12vw;
+  margin: 0px;
 }
-#bottom_text
+#gmail
 {
-  font-size: 4em;
-  margin: 3vw;
-  color: rgb(26, 26, 26);
-  
-  grid-column: 1;
-  grid-row: 1;
-  pointer-events: none;
+  color: #d93025;
+  clip-path: circle(0% at 0% 0%)
 }
-#mail:hover #top_text
+#twitter
 {
-  clip-path: polygon(0% 0%, 125% 0%, 100% 100%, 0% 100%);  
+  color: #1da1f2;
+  clip-path: circle(0% at 0% 0%)
 }
 </style>
